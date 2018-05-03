f332cbcb9021555926f47a90f871308cfaa59a31
kharazzoro <kha.1991.14.2017@gmail.com>
2018-05-03 10:34:44 +0100

    update adminDashboard function to be pure function

--- web/src/components/views/AdminDashboard.js
+++ web/src/components/views/AdminDashboard.js
@@ -1,64 +1,63 @@
-import React, { Component, Fragment } from "react";
+import React from "react";
 import { Col, Row, Container } from "reactstrap";
 import "../../assets/css/view/AdminDashBoard.css";
 import { spawn } from "child_process";
 
-export default class AdminDashboard extends Component {
-  render() {
-    const { location } = this.props;
-    const { user } = this.props;
-    return (
-      <Container className="admin-dashboard">
-        <Row>
-          {user && !user.admin && location.pathname !== "/" ? (
-            <Col sm={3} />
-          ) : null}
-          {user &&
-          user.admin &&
-          location.pathname !== "/" &&
-          location.pathname !== "/userAgreement" &&
-          location.pathname !== "/privacyPolicy" &&
-          location.pathname !== "/faq" &&
-          location.pathname !== "/feedback" ? (
-            <Col xs="12">
-              <span
-                onClick={() => {
-                  this.props.history.push({
-                    pathname: "/approve-users"
-                  });
-                }}
-              >
-                Approve Users
-              </span>
-            </Col>
-          ) : null}
-        </Row>
+const AdminDashboard=(props)=>{
+  const { location } = props;
+  const { user } = props;
+  return(
+    <Container className="admin-dashboard">
+      <Row>
+        {user && !user.admin && location.pathname !== "/" ? (
+          <Col sm={3} />
+        ) : null}
+        {user &&
+        user.admin &&
+        location.pathname !== "/" &&
+        location.pathname !== "/userAgreement" &&
+        location.pathname !== "/privacyPolicy" &&
+        location.pathname !== "/faq" &&
+        location.pathname !== "/feedback" ? (
+          <Col xs="12">
+            <span
+              onClick={() => {
+                 props.history.push({
+                  pathname: "/approve-users"
+                });
+              }}
+            >
+              Approve Users
+            </span>
+          </Col>
+        ) : null}
+      </Row>
 
-        <Row>
-          {user && !user.admin && location.pathname !== "/" ? (
-            <Col sm={3} />
-          ) : null}
-          {user &&
-          user.admin &&
-          location.pathname !== "/" &&
-          location.pathname !== "/userAgreement" &&
-          location.pathname !== "/privacyPolicy" &&
-          location.pathname !== "/faq" &&
-          location.pathname !== "/feedback" ? (
-            <Col xs="12">
-              <span
-                onClick={() => {
-                  this.props.history.push({
-                    pathname: "/admin-view-users-menu"
-                  });
-                }}
-              >
-                View Users
-              </span>
-            </Col>
-          ) : null}
-        </Row>
-      </Container>
-    );
-  }
+      <Row>
+        {user && !user.admin && location.pathname !== "/" ? (
+          <Col sm={3} />
+        ) : null}
+        {user &&
+        user.admin &&
+        location.pathname !== "/" &&
+        location.pathname !== "/userAgreement" &&
+        location.pathname !== "/privacyPolicy" &&
+        location.pathname !== "/faq" &&
+        location.pathname !== "/feedback" ? (
+          <Col xs="12">
+            <span
+              onClick={() => {
+                 props.history.push({
+                  pathname: "/admin-view-users-menu"
+                });
+              }}
+            >
+              View Users
+            </span>
+          </Col>
+        ) : null}
+      </Row>
+    </Container>
+  );
 }
+ export default  AdminDashboard;
